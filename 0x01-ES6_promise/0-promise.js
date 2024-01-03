74b560c9e210bf3a165ebd228bb2243c34cd5e78
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-03 20:27:06 +0000

    promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
+
 export default function getResponseFromAPI() {
-	  return new Promise(() => {});
+  return new Promise((res, rej) => {
+    res('Hi Mom');
+    rej('Rejected');
+  });
 }
