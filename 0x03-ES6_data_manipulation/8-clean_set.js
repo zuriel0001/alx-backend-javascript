094cf1da993443fda75a97359a0e6869edad64bf
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-09 23:13:36 +0100

    clean sheet

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+/**
+ * returns a string of all the set values that start with a specific string
+ */
+
+const cleanSet = (set, startString) => {
+	  const strings = [];
+
+	  if (startString === '' || typeof startString !== 'string') return '';
+	  set.forEach((s) => {
+		      if (typeof s === 'string' && s.startsWith(startString)) {
+			            strings.push(s.slice(startString.length));
+			          }
+		    });
+	  return strings.join('-');
+};
+
+export default cleanSet;
