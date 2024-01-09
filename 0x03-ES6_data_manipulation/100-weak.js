7607ca8c537a4d119fa74e1c5fef04851fcef7e1
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-09 20:44:00 +0000

    advance

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+/**
+ *  weak map of endpoints and the number of calls made.
+ */
+
+const weakMap = new WeakMap();
+
+const queryAPI = (endpoint) => {
+  let total = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, total -= -1);
+  if (total >= 5) throw new Error('Endpoint load is high');
+  return total;
+};
+
+export { weakMap, queryAPI };
