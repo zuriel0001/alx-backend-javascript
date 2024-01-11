67ee91f5141353ef07fed8fd483bcd7f57ddb3eb
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-11 15:18:29 +0000

    update

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,13 +2,12 @@
  * Changes the quantity of unique grocery items to 100
  */
 
-export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
   map.forEach((value, key) => {
-    if (value === 1) {
-      map.set(key, 100);
-    }
+    if (value === 1) map.set(key, 100);
   });
-}
+};
+
+export default updateUniqueItems;
